a1c3d66f3e3220a9732871508297f084795c0547
mrmateussiilva <mateuspython3010@gmail.com>
2024-11-04 21:04:53 +0000

    Melhorando a entrada de dados

--- estoque.py
+++ estoque.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+from materiais import PAPEIS
 
 
 estoque_locais = "estoque_locais.csv"
@@ -14,6 +15,24 @@ def load_data(file):
         return pd.DataFrame(columns=["Nome", "Quantidade(unidade)"])
 
 st.title("Controle de Estoque -SAF-v0.0.1")
+with st.sidebar:
+    select_tipo_entrada = st.selectbox(
+        "Selecione o Tipo de Material",
+        options=["","Papel","Tinta","Tecido"]
+    )
+
+    if select_tipo_entrada == "Tinta":
+        st.write("Selecionando tinta")
+    elif select_tipo_entrada == "Papel":
+        tipos_tecidos = st.selectbox(
+            "Selecione o Papel",
+            options=PAPEIS
+        )
+        metros = st.number_input("Quantide(Metros no Rolo)",placeholder="100",min_value=10,step=10)
+        qtd_unidade = st.number_input("Quantide(Unidade)",placeholder="1",min_value=1,step=1)
+        
+    elif select_tipo_entrada == "Tecido":
+        st.write("Selecionando Tecido")
 
 data_locais = load_data(file=estoque_locais)
 data_papeis = load_data(file=estoque_papeis)
---
+++ materiais.py
@@ -0,0 +1,8 @@
+
+
+PAPEIS = [
+    "Papel Art 45gr - 160",
+    "Papel Art 45gr - 180",
+    "Papel Kraft 35gr - 160",
+    "Papel SEDA 20gr - 160",
+]
