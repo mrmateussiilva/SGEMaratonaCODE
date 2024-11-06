c4633c2eaa158d43440b683012c9c511802e4a3c
mrmateussiilva <mateuspython3010@gmail.com>
2024-11-06 04:12:05 +0000

    Refatorando e restruturando o core do projeto

--- estoque.py
+++ estoque.py
@@ -1,46 +1,93 @@
 import streamlit as st
 import pandas as pd
-import os
+
 from materiais import PAPEIS
+from utils import load_data
 
 
-estoque_locais = "estoque_locais.csv"
-estoque_papeis = "estoque_papeis.csv"
+data_estoque = {
+    "tecido_locais":load_data("model_csv/estoque_locais.csv"),
+    "papeis":load_data("model_csv/estoque_papeis.csv"),
+    "tecidos":load_data("model_csv/estoque_papeis.csv"),
+    "tintas":load_data("model_csv/estoque_tintas.csv"),
+}
 
 
-def load_data(file):
-    if os.path.exists(file):
-        return pd.read_csv(file)
-    else:
-        return pd.DataFrame(columns=["Nome", "Quantidade(unidade)"])
+TIPOS_PAPEIS = set(map(lambda x:x,data_estoque["papeis"]["Nome"]))
+TIPOS_LOCAIS = set(map(lambda x:x,data_estoque["tecido_locais"]["Nome"]))
+NOMES_TINTAS = set(map(lambda x:x,data_estoque["tintas"]["Cor"]))
+TIPOS_TINTAS = set(map(lambda x:x,data_estoque["tintas"]["Tipo"]))
 
-st.title("Controle de Estoque -SAF-v0.0.1")
+
+st.title("Controle de Estoque -SAF-v0.0.3")
+
+
 with st.sidebar:
     select_tipo_entrada = st.selectbox(
         "Selecione o Tipo de Material",
         options=["","Papel","Tinta","Tecido"]
     )
 
+        #  TINTA
     if select_tipo_entrada == "Tinta":
-        st.write("Selecionando tinta")
+        cor_tinta = st.selectbox(
+            "Selecione a Cor",
+            options=NOMES_TINTAS
+        )
+        tipo_tinta = st.selectbox(
+            "Tipo de tint",
+            options=TIPOS_TINTAS
+        )
+        qtd_unidade = st.number_input("Quantide(Unidade)",placeholder="1",min_value=1,step=1)
+        qtd_litros = st.number_input("Quantide(Litros)",placeholder="1",min_value=1,step=1)
+        submit = st.button("Inserir")
+
+
     elif select_tipo_entrada == "Papel":
         tipos_tecidos = st.selectbox(
             "Selecione o Papel",
-            options=PAPEIS
+            options=TIPOS_PAPEIS
         )
-        metros = st.number_input("Quantide(Metros no Rolo)",placeholder="100",min_value=10,step=10)
+        metros = st.number_input("Quantide(Metros no Rolo)",placeholder="100",min_value=250,step=10)
         qtd_unidade = st.number_input("Quantide(Unidade)",placeholder="1",min_value=1,step=1)
-        
+        submit = st.button("Inserir")
+
+
+    elif select_tipo_entrada == "Tecido Locais":
+        tipos_tecidos = st.selectbox(
+            "Selecione o Tecido Local",
+            options=TIPOS_LOCAIS
+        )
+        qtd_unidade = st.number_input("Quantide(Unidade)",placeholder="1",min_value=1,step=1)
+        submit = st.button("Inserir")
+
     elif select_tipo_entrada == "Tecido":
         st.write("Selecionando Tecido")
 
-data_locais = load_data(file=estoque_locais)
-data_papeis = load_data(file=estoque_papeis)
 
 
-st.subheader("Estoque Tecido Locais")
-st.dataframe(data_locais)
-st.subheader("Estoque Papeis")
-st.dataframe(data_papeis)
+def view_previsão():
+    st.title("Previsão de gasto de materias")
 
 
+def view_gastos():
+    st.title("Previsão de gasto de materias")
+
+
+def view_data_estoque():
+    st.subheader("Estoque Tecido Locais")
+    st.data_editor(data_estoque["tecido_locais"])
+    st.subheader("Estoque Papeis")
+    st.data_editor(data_estoque["papeis"])
+
+
+
+
+pg = st.navigation([
+    st.Page(view_data_estoque, title="Estoque", icon=""),
+    st.Page(view_gastos, title="Gastos", icon=""),
+    st.Page(view_previsão, title="Previsão de Gastos", icon=""),
+])
+
+
+pg.run()
