fedd843457380ae68413047d9bd20c13f09fb897
MatheusM4160 <matheusmcantarela@gmail.com>
2025-01-25 09:43:54 -0300

    Tirando arquivos desnecessarios

--- main.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.write("Principal (Não sei oq colocar aqui ainda)")
--- materiais.py
+++
@@ -1,8 +0,0 @@
-
-
-PAPEIS = [
-    "Papel Art 45gr - 160",
-    "Papel Art 45gr - 180",
-    "Papel Kraft 35gr - 160",
-    "Papel SEDA 20gr - 160",
-]
