a31970275d2ed74dc604ff7bb7c420aca830f23c
MatheusM4160 <matheusmcantarela@gmail.com>
2025-01-28 16:20:52 -0300

    Atualização do css, importação das inspirações e algumas alterações no 'base.html'

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,10 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
+@app.route('/fornecedores')
 def index():
     return render_template('fornecedores.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
