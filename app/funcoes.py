16ccfb7f31f11cc722fc8bbb85016a095113bc42
MatheusM4160 <matheusmcantarela@gmail.com>
2025-01-29 14:25:25 -0300

    Terminado função para excluir linha da tabela

--- app/funcoes.py
+++ app/funcoes.py
@@ -1,8 +1,12 @@
 import json
 
-def Fornecedores():    
+def LerFornecedores():    
     with open('app/db/fornecedores.json', 'r') as file:
         db = json.load(file)
         file.close()
 
     return db
+
+def SalvarFornecedores(fornecedores):
+    with open("app/db/fornecedores.json", "w", encoding="utf-8") as f:
+        json.dump(fornecedores, f, indent=4, ensure_ascii=False)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,35 @@
 from app import app
-from flask import render_template
-from app.funcoes import Fornecedores
+from flask import render_template, redirect ,url_for
+from app.funcoes import LerFornecedores, SalvarFornecedores
 
 @app.route('/')
 @app.route('/fornecedores')
 def index():
-    fornecedores_db= Fornecedores()
+    fornecedores_db= LerFornecedores()
     return render_template('fornecedores.html', fornecedores=fornecedores_db)
+
+@app.route('/excluir_fornecedor/<int:id>')
+def excluir_fornecedor(id):
+    fornecedores_db = LerFornecedores()
+
+    fornecedores_db = [f for f in fornecedores_db if f['id'] != id]
+
+    for index, fornecedor in enumerate(fornecedores_db, start=0):
+        fornecedor['id'] = index
+    
+    SalvarFornecedores(fornecedores_db)
+
+    return redirect(url_for('index'))
+
+@app.route('/adicionar_fornecedores')
+def adicionar_fornecedores():
+    return render_template('adicionar_fornecedores.html')
+    
+@app.route('/editar_fornecedor')
+def editar_fornecedor():
+    return render_template('editar_fornecedor.html')
 
 @app.route('/perfil')
 def perfil():
     return render_template('perfil.html')
+
